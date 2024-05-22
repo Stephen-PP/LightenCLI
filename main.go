db4450ab850104a9b67d222401eff8b7b3382211
Stephen-PP <pierrepaulstephen@gmail.com>
2024-05-22 16:39:09 -0400

    fixing quick var name change

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 )
 
 func main() {
-	losslessOpts := functions.LosslessOptions{Level: 7, Strip: true, Interlace: true, OutputFilename: "./test_files/outputs/output-lossless.png"}
+	losslessOpts := functions.LosslessPngOptions{Level: 7, Strip: true, Interlace: true, OutputFilename: "./test_files/outputs/output-lossless.png"}
 	_ = functions.CompressPNGLossless("./test_files/image.png", losslessOpts)
 
 	// Lossy
-	lossyOpts := functions.LossyOptions{Quality: "65-80", Speed: 5, OutputFilename: "./test_files/outputs/output-lossy.png"}
+	lossyOpts := functions.LossyPngOptions{Quality: "65-80", Speed: 5, OutputFilename: "./test_files/outputs/output-lossy.png"}
 	_ = functions.CompressPNGLossy("./test_files/image.png", lossyOpts)
 }
